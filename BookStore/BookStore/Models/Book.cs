a4445c81c31b77315211c051b081f7c6cb6d2ea4
Rahim Pormah <sas4n.porm4h@gmail.com>
2023-08-02 15:09:14 +0200

    Add project files.

---
+++ BookStore/BookStore/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace BookStore.Models
+{
+    public class Book
+    {
+
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+
+    }
+}
