abb4321f3b92d4da6c95c2aec4785446f096e40b
Rahim Pormah <sas4n.porm4h@gmail.com>
2023-08-03 01:55:05 +0200

    Get and Post merhods fixed

---
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,38 @@
+﻿using BookStore.DB;
+using BookStore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookDbContext _bookDbContext;
+
+        public BooksController(BookDbContext bookDbContext)
+        {
+            _bookDbContext = bookDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllBooks ()
+        {
+            var Books = await _bookDbContext.Books.ToListAsync();
+
+            return Ok(Books);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddBook (Book newBook)
+        {
+            newBook.Id = Guid.NewGuid();
+            await _bookDbContext.Books.AddAsync(newBook);
+            await _bookDbContext.SaveChangesAsync();
+
+            return Ok(newBook);
+        }
+    }
+}
