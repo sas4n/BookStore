3b0db8ccc10abe6c43862a85c5a2ba88e0b9e465
Rahim Pormah <sas4n.porm4h@gmail.com>
2023-08-02 15:09:14 +0200

    Model and db added

---
+++ BookStore/BookStore/DB/BookDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.DB
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
