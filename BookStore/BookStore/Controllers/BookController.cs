757c53a736a75b98e37aea57b96d909482e8fff4
Rahim Pormah <sas4n.porm4h@gmail.com>
2023-08-03 16:54:44 +0200

    GetBook and Put methods added

--- BookStore/BookStore/Controllers/BookController.cs
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -18,7 +18,7 @@ namespace BookStore.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAllBooks ()
+        public async Task<IActionResult> GetAllBooks()
         {
             var Books = await _bookDbContext.Books.ToListAsync();
 
@@ -26,13 +26,47 @@ namespace BookStore.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddBook (Book newBook)
+        public async Task<IActionResult> AddBook([FromBody] Book newBook)
         {
             newBook.Id = Guid.NewGuid();
             await _bookDbContext.Books.AddAsync(newBook);
             await _bookDbContext.SaveChangesAsync();
 
             return Ok(newBook);
+        }
+
+        [HttpGet("{id:Guid}")]
+        public async Task<IActionResult> GetBookById([FromRoute] Guid id)
+        {
+            Book book = await _bookDbContext.Books.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(book == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(book);
+        }
+
+        [HttpPut("{id:Guid}")]
+        public async Task<IActionResult> UpdateBook([FromBody] Book newBook, Guid id)
+        {
+           if(newBook.Id != id) { return BadRequest(); }
+
+            Book book = await _bookDbContext.Books.FindAsync(id);
+            
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            book.Author = newBook.Author;
+            book.Title = newBook.Title;
+            book.PublicationDate = newBook.PublicationDate;
+
+            await _bookDbContext.Books.AddAsync(book);
+            await _bookDbContext.SaveChangesAsync();
+            return Ok(book);
         }
     }
 }
